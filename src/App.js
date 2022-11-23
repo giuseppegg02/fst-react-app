e5620dab9e633173fa025837a7362f4e744b8906
Giuseppe Rignanese <qwertyuiop.gr97@gmail.com>
2022-11-23 10:21:02 +0100

    app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 
 // function component / componente funzionale
 function App() {
-  let numero = 10
+  let numero = 70
   return (
     <div className="App">
-      ciao 10+10 {numero}
+      10+10
+      <h1> dovrebbe essere solo 70 {numero}</h1>
+
+         dovrebbe essere 80 {numero + 10}
+ 
+      <div>
+        <h1>{numero}</h1>
+      </div>
+
     </div>
   );
 }
 
 export default App;
+  
+function stampaCiao(){
+  console.log("ciao")
+}
